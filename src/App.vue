dedfea4247485efba505c4cf53415770d5f93bc4
Jarrod Whitley <jarrodwhitley@gmail.com>
2023-10-09 22:30:42 -0500

    play button working

--- src/App.vue
+++ src/App.vue
@@ -45,14 +45,3 @@ export default {
 
 }
 </script>
-
-
-
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.scss'
-import './assets/js/all.min.js';
+// import './assets/js/all.min.js';
 import App from './App.vue'
 
 createApp(App).mount('#app')
