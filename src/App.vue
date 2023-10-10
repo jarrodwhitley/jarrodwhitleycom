c81a2566a15a335f295f99095ace1450f934d3da
Jarrod Whitley <jarrodwhitley@gmail.com>
2023-10-09 22:09:05 -0500

    swap to Vue3

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+        <img class="logo" width="80" src="/src/assets/jw-logo.svg" alt="logo"/>
+        <div class="links">
+            <a href="https://linkedin.com/jarrodwhitley"><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+    </header>
+    <div id="content">
+        <section class="intro">
+            <div class="text">
+                <h1 class="page-title">I don't <span class="glitch" data-text="hack">hack</span>
+                    mainframes</h1>
+                <p class="page-subtitle">But I <em>do</em> listen to synthwave while building web apps</p>
+                <div class="btn">View in style</div>
+            </div>
+            <div class="grid-bg">
+                <div class="grid-lines"></div>
+            </div>
+            <div class="crt-lines"></div>
+            <div class="crt-darken"></div>
+            <div class="crt-vignette"></div>
+        </section>
+        <section class="experience" height="500" width="100%">
+            <h2>A little about me</h2>
+        </section>
+    </div>
+    <AudioPlayer @playing="hello"/>
+</template>
+
+<script>
+import AudioPlayer from './components/AudioPlayer.vue'
+
+export default {
+    components: {
+        AudioPlayer
+    },
+    setup() {
+        return {  }
+    },
+    methods: {
+        hello() {
+            alert('hello');
+        }
+    }
+
+}
+</script>
+
+
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.scss'
+import './assets/js/all.min.js';
+import App from './App.vue'
+
+createApp(App).mount('#app')
