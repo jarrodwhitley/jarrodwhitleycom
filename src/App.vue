04431ed1618c54536a8776327c1844b56e0ffa14
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-11-05 14:21:54 -0600

    fixed app.vue

--- src/App.vue
+++ src/App.vue
@@ -5,40 +5,15 @@
             <a href="https://linkedin.com/jarrodwhitley"><i class="fa-brands fa-vuejs"></i></a>
             <a class="disabled" href="https://linkedin.com/jarrodwhitley"><i class="fa-brands fa-react"></i></a>
             <a href="https://linkedin.com/jarrodwhitley"><i class="fa-brands fa-linkedin"></i></a>
-            <a href="https://linkedin.com/jarrodwhitley"><i class="fa-brands fa-soundcloud"></i></a>
-            <a href="https://github.com/jarrodwhitley"><i class="fa-brands fa-github"></i></a>
+            <a href="https://github.com/jarrodwhitley"><i class="fa-brands fa-github"></i>></a>
         </div>
     </header>
     <section id="content" :class="{ 'no-animations': isLoading }">
         <section class="intro">
             <div class="text">
-                <!-- <h1 class="page-title">I don't <span class="glitch" data-text="hack">hack</span>
-                    mainframes</h1>
-                <p class="page-subtitle">But I do listen to synthwave while building web apps</p> -->
-                <div class="card-grid">
-                    <div class="card">
-                        <h3>Music</h3>
-                        <div class="card__item">
-                            <i class="fa-brands fa-apple"></i>
-                            <span>Apple Music</span>
-                        </div>
-                        <div class="card__item">
-                            <i class="fa-brands fa-spotify"></i>
-                            <span>Spotify Music</span>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <h3>Music</h3>
-                        <div class="card__item">
-                            <i class="fa-brands fa-apple"></i>
-                            <span>Apple Music</span>
-                        </div>
-                        <div class="card__item">
-                            <i class="fa-brands fa-spotify"></i>
-                            <span>Spotify Music</span>
-                        </div>
-                    </div>
-                </div>
+                <h1 class="page-title">I don't <span class="glitch" data-text="hack">hack</span>
+                                       mainframes</h1>
+                <p class="page-subtitle">But I do listen to synthwave while building web apps</p>
             </div>
             <div class="grid-bg">
                 <div class="grid-lines"></div>
@@ -46,12 +21,163 @@
             <div class="crt-lines"></div>
             <div class="crt-darken"></div>
             <div class="crt-vignette"></div>
+        </section>
+        <section class="experience">
+            <h2 class="skillset">My Skillset</h2>
+            <SkillChart></SkillChart>
+            <h2 class="hobbies">My Hobbies</h2>
+            <HobbyChart></HobbyChart>
+        </section>
+        <section class="data-driven" v-if="quotes" :class="{ 'hood-open': hoodOpen }">
+            <h2>Data-driven Applications</h2>
+            <!--            <div class="sub-heading" v-observe-visibility="dataSectionOptions">using the<a class="link"-->
+            <!--                   :class="{ 'glitch': isHovering }"-->
+            <!--                   @mouseover="isHovering = true"-->
+            <!--                   @mouseleave="isHovering = false"-->
+            <!--                   data-text="DummyJSON API"-->
+            <!--                   href="https://github.com/Ovi/DummyJSON"-->
+            <!--                   target="_blank">DummyJSON API</a></div>-->
+            <div class="data-tabs" v-observe-visibility="dataSectionOptions">
+                <div class="data-tab" v-for="tab in dataTabs"
+                     :id="tab.data"
+                     :class="{ 'glitch': tab.isHovering, 'active': tab.isActive }"
+                     @mouseover="mouseOverHandler(tab)"
+                     @mouseleave="mouseLeaveHandler(tab)"
+                     @click="changeDataTab(tab)"
+                     :data-text="tab.name"
+                     v-text="tab.name"></div>
+            </div>
+            <div class="birdle carousel-item animate__animated" :class="dataCarouselClasses('birdle')">
+                <div id="birdleApp" @click="birdleFocus">
+                    <div class="birdle-grid animate__animated" :class="{ 'fade': birdle.modal.show }">
+                        <div class="attempt row animate__animated"
+                             :class="{ 'animate__heartBeat': attempt === birdle.bird }"
+                             v-for="(attempt, index) in birdle.attempts"
+                             :key="index">
+                            <div class="letter"
+                                 :class="letterCheck(letter, index)"
+                                 v-for="(letter, index) in attempt.split('')"
+                                 :key="index"
+                                 v-text="letter"></div>
+                        </div>
+                        <div class="guess row" v-if="remainingGuesses">
+                            <div class="guess letter animate__animated animate__faster"
+                                 :class="{ 'animate__pulse': letter }"
+                                 v-for="(letter, index) in guessLettersArray" v-text="letter"></div>
+                        </div>
+                        <div class="empty row"
+                             v-if="remainingGuesses"
+                             v-for="guess in (remainingGuesses - 1)">
+                            <div class="empty letter" v-for="letter in 5"></div>
+                        </div>
+                    </div>
+                    <div class="birdle-modal animate__animated" :class="this.birdle.modal.show ? 'animate__bounceIn' : 'animate__bounceOut'">
+                        <div class="birdle-modal__container" >
+                            <div class="birdle-modal__title" v-text="modalHeading"></div>
+                            <div class="birdle-modal__content" v-text="modalContent"></div>
+                            <div v-if="birdle.gameWon" class="share-link-btn" :class="{ 'copied': birdle.linkCopied }" @click="copyShareLink" v-text="birdle.linkCopied ? 'Link Copied!' : 'Copy Share Link'"></div>
+                            <em v-if="birdle.gameLost">The answer was "{{ birdle.bird }}"</em>
+                        </div>
+                        <div class="modal-close" @click="closeBirdleModal">
+                            <i class="fa-solid fa-times"></i>
+                        </div>
+                    </div>
+                    <div class="legend-row">
+                        <div class="legend-item">
+                            <div class="legend-item__color correct-position"></div>
+                            <div class="legend-item__text">Correct Position</div>
+                        </div>
+                        <div class="legend-item">
+                            <div class="legend-item__color correct-letter"></div>
+                            <div class="legend-item__text">Correct Letter</div>
+                        </div>
+                        <div class="legend-item">
+                            <div class="legend-item__color wrong-letter"></div>
+                            <div class="legend-item__text">Wrong Letter</div>
+                        </div>
+                    </div>
+                    <div class="input" v-if="!birdle.gameOver" onkeydown="return /[a-z]/i.test(event.key)">
+                        <input v-model="birdle.guess" ref="birdleInput" maxlength="5" type="text" @keydown.enter.prevent="enterPress"/>
+                        <button @click="checkWord">Submit</button>
+                    </div>
+                    <div class="restart button" v-if="birdle.gameOver">
+                        <div @click="birdleReset">Play Again</div>
+                    </div>
+                </div>
+            </div>
+            <div class="chat carousel-item animate__animated" :class="dataCarouselClasses('chat')">
+                <div class="chat-container">
+                    <div class="chat-container__header">
+                        <div class="chat-container__header__avatar">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" id="svg2">
+                                <defs id="defs4"/>
+                                <g transform="translate(-158.23886,-935.57002)" id="layer1">
+                                    <g id="g2171">
+                                        <path d="M 411.42102 938.01221 A 91.061981 91.061981 0 1 1  229.29706,938.01221 A 91.061981 91.061981 0 1 1  411.42102 938.01221 z" transform="matrix(0.1757045,0,0,0.1757045,117.95033,786.75706)" style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1" id="path2162"/>
+                                        <path d="M 217.95163 911.14148 A 38.51474 38.51474 0 1 1  140.92215,911.14148 A 38.51474 38.51474 0 1 1  217.95163 911.14148 z" transform="matrix(0.1757045,0,0,0.1757045,143.02574,786.12755)" style="fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:1;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1" id="path3136"/>
+                                        <path d="M 74.226563,890.49332 C 71.601552,889.53241 69.656242,888.16132 68.390625,886.38004 C 67.124994,884.59882 66.492183,882.46601 66.492188,879.9816 C 66.492183,876.23165 67.839837,873.0793 70.535156,870.52457 C 73.230457,867.96993 76.816391,866.69259 81.292969,866.69254 C 85.792944,866.69259 89.414035,867.99923 92.15625,870.61246 C 94.898404,873.22579 96.269496,876.40743 96.269531,880.15738 C 96.269496,882.54804 95.642544,884.62812 94.388672,886.39762 C 93.134734,888.16718 91.230439,889.53241 88.675781,890.49332 C 91.839813,891.5246 94.248014,893.18866 95.900391,895.48551 C 97.552698,897.7824 98.378869,900.52459 98.378906,903.71207 C 98.378869,908.11833 96.820277,911.82145 93.703125,914.82145 C 90.585908,917.82145 86.48435,919.32145 81.398438,919.32145 C 76.312485,919.32145 72.210927,917.81559 69.09375,914.80387 C 65.976558,911.79216 64.417966,908.0363 64.417969,903.53629 C 64.417966,900.18475 65.267574,897.37811 66.966797,895.11637 C 68.666008,892.85468 71.085928,891.31366 74.226563,890.49332 L 74.226563,890.49332 z M 72.960938,879.77067 C 72.960926,882.2082 73.746082,884.20039 75.316406,885.74723 C 76.886703,887.29413 78.925764,888.06757 81.433594,888.06754 C 83.871071,888.06757 85.869116,887.29999 87.427734,885.76481 C 88.986301,884.22968 89.765597,882.34883 89.765625,880.12223 C 89.765597,877.80196 88.962863,875.85079 87.357422,874.26871 C 85.751929,872.68673 83.753884,871.89571 81.363281,871.89567 C 78.949201,871.89571 76.945297,872.66915 75.351563,874.21598 C 73.7578,875.7629 72.960926,877.61446 72.960938,879.77067 L 72.960938,879.77067 z M 70.921875,903.57145 C 70.921866,905.37615 71.3496,907.12224 72.205078,908.80973 C 73.060535,910.49724 74.332018,911.80388 76.019531,912.72965 C 77.707015,913.65544 79.523419,914.11833 81.46875,914.11832 C 84.492165,914.11833 86.988256,913.14567 88.957031,911.20035 C 90.925752,909.25505 91.910126,906.7824 91.910156,903.78238 C 91.910126,900.73553 90.896455,898.216 88.869141,896.22379 C 86.841772,894.23163 84.304665,893.23553 81.257813,893.23551 C 78.281233,893.23553 75.814439,894.21991 73.857422,896.18863 C 71.90038,898.1574 70.921866,900.61834 70.921875,903.57145 L 70.921875,903.57145 z " transform="matrix(0.1739794,2.4560695e-2,-2.4560695e-2,0.1739794,182.20067,788.39671)" style="font-size:72px" id="flowRoot4206"/>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="chat-container__header__username">8 Ball Chat</div>
+                        <div class="chat-container__header__status"></div>
+                    </div>
+                    <div class="chat-container__body">
+                        <div class="chat-container__body__message" :class="chat.user ? 'user' : 'bot'" v-for="chat in chatLog">
+                            <div class="chat-container__body__message__avatar"></div>
+                            <div class="chat-container__body__message__text" v-text="chat.message"></div>
+                        </div>
+                    </div>
+                    <svg class="chat-container__loading-dots animate__animated" :class="botThinking ? 'animate__fadeIn' : 'animate__fadeOut'" id="dots" width="4rem" height="1rem" viewBox="0 0 132 58" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                            <g id="dots" sketch:type="MSArtboardGroup" fill="#A3A3A3">
+                                <circle id="dot1" sketch:type="MSShapeGroup" cx="25" cy="30" r="13"></circle>
+                                <circle id="dot2" sketch:type="MSShapeGroup" cx="65" cy="30" r="13"></circle>
+                                <circle id="dot3" sketch:type="MSShapeGroup" cx="105" cy="30" r="13"></circle>
+                            </g>
+                        </g>
+                    </svg>
+                    <div class="chat-container__footer">
+                        <input type="text" ref="chatInput" v-model="userInput" placeholder="Type a message..."/>
+                        <div class="chat-container__footer__send" @click="createUserMessage">Send</div>
+                    </div>
+                </div>
+            </div>
+            <div class="bamazon carousel-item animate__animated" :class="dataCarouselClasses('bamazon')">
+
+            </div>
+            <div class="next-btn"></div>
+            <!--            <div class="btn btn-glow&#45;&#45;neon" @click="toggleQuotesHood" v-text="hoodOpen ? 'Close Hood' : 'See Under the Hood'"></div>-->
+        </section>
+        <section class="ai-friendly">
+            <h2>Pair Programming</h2>
+            <div class="sub-heading">with
+                <a class="link"
+                   :class="{ 'glitch': isHovering }"
+                   @mouseover="isHovering = true"
+                   @mouseleave="isHovering = false"
+                   data-text="GitHub Copilot"
+                   href="https://github.com/features/copilot"
+                   target="_blank">GitHub Copilot</a></div>
+            <div class="section-content" v-observe-visibility="(isVisible, entry) => visibilityChanged(isVisible, entry, 'aiSectionIsVisible')">
+                <div class="icons">
+                    <img class="copilot animate__animated" :class="aiSectionIsVisible ? 'animate__fadeInUp' : 'animate__fadeOutDown'" src="/src/assets/images/github-copilot.png" alt="copilot"/>
+                    <img class="chatgpt animate__animated" :class="aiSectionIsVisible ? 'animate__fadeInUp' : 'animate__fadeOutDown'" src="/src/assets/images/chatgpt.jpg" alt="chatgpt"/>
+                    <img class="midjourney animate__animated" :class="aiSectionIsVisible ? 'animate__fadeInUp' : 'animate__fadeOutDown'" src="/src/assets/images/midjourney.png" alt="midjourney"/>
+                </div>
+                <div class="text">
+                    <h3>I consider myself to be an AI enthusiast</h3>
+                    <p>I've dabbled in AI image generation with services like Midjourney and Adobe's new generative fill. I've used ChatGPT to create a JSON schema from a block of HTML data. Most of my time has been spent working with Github's new AI-pair programming tool, Copilot. It's certainly a mixed bag in terms of what it does well and not so well, but I have high hopes for Copilot.</p>
+                    <p>Most importantly, I believe the future of programming will be AI-assisted programming. As these tools get better and better over time they will help software engineers to become more efficient.</p>
+                </div>
+            </div>
             <div class="circuit-board"></div>
         </section>
     </section>
     <footer>
         <h4>Acknowledgements</h4>
-        <!-- <span>Circuit board animation by<a href="https://codepen.io/Temmer/full/ExjNJog" target="_blank">Temmer Péter</a></span> -->
+        <span>Circuit board animation by<a href="https://codepen.io/Temmer/full/ExjNJog" target="_blank">Temmer Péter</a></span>
         <span>Synthwave Grid by<a href="https://codepen.io/pierredarrieutort/pen/Vwaoqqe" target="_blank">Pierre Darrieutort</a></span>
         <span>Glitch text by<a href="https://codepen.io/lbebber/pen/nqwBKK" target="_blank">Lucas Bebber</a></span>
     </footer>
@@ -118,14 +244,14 @@ export default {
                 {
                     id: 0,
                     name: 'Birdle',
-                    isActive: true,
+                    isActive: false,
                     data: 'birdle',
                     isHovering: false
                 },
                 {
                     id: 1,
                     name: '8 Ball Chat',
-                    isActive: false,
+                    isActive: true,
                     data: 'chat',
                     isHovering: false
                 },
@@ -161,24 +287,23 @@ export default {
                 linkCopied: false,
             },
             bamazon: {
-                show: false,
                 products: [],
             }
         }
     },
     created() {
         fetch('https://dummyjson.com/products')
-        .then(res => res.json())
-        .then(json => this.bamazon.products = json.products);
+            .then(res => res.json())
+            .then(json => this.bamazon.products = json.products);
         fetch('https://dummyjson.com/users')
-        .then(res => res.json())
-        .then(json => this.users = json.users);
+            .then(res => res.json())
+            .then(json => this.users = json.users);
         fetch('https://dummyjson.com/comments')
-        .then(res => res.json())
-        .then(json => this.comments = json.comments);
+            .then(res => res.json())
+            .then(json => this.comments = json.comments);
         fetch('https://dummyjson.com/quotes')
-        .then(res => res.json())
-        .then(json => this.quotes = json.quotes);
+            .then(res => res.json())
+            .then(json => this.quotes = json.quotes);
     },
     mounted() {
         window.addEventListener('load', () => {
@@ -206,9 +331,9 @@ export default {
         checkWord() {
             let guess = this.birdle.guess;
             this.axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`)
-            .then(response => {
-                if (response.data[0].word === guess) this.makeGuess(guess);
-            }).catch(error => {
+                .then(response => {
+                    if (response.data[0].word === guess) this.makeGuess(guess);
+                }).catch(error => {
                 this.birdle.modal.notAWord = true;
                 this.birdle.modal.show = true;
             });
@@ -358,10 +483,6 @@ export default {
             }, 2000);
 
         },
-        closeBamazon() {
-            console.log('closing bamazon');
-            this.bamazon.show = false;
-        }
     },
     computed: {
         activeDataTab() {
@@ -419,10 +540,4 @@ export default {
         // }
     }
 }
-// on keypress if key is "s" alert("s")
-document.addEventListener('keydown', (event) => {
-    if (event.keyCode === 83) {
-        alert('s');
-    }
-});
 </script>
