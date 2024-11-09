f0fcca9a16a3b14d3078906ef88276bdee238931
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-11-09 15:07:11 -0600

    birdle component fully broken out

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,7 @@
                      v-text="tab.name"></div>
             </div>
             <div class="birdle carousel-item animate__animated" :class="dataCarouselClasses('birdle')">
-                <Birdle :word-bank="wordBank"/>
+                <Birdle/>
             </div>
             <div class="chat carousel-item animate__animated" :class="dataCarouselClasses('chat')">
                 <div class="chat-container">
@@ -304,15 +304,6 @@ export default {
             this.isLoading = false;
         });
         window.addEventListener('scroll', this.handleScroll);
-        document.addEventListener('keydown', (event) => {
-            if (event.keyCode === 13 && this.$refs.chatInput === document.activeElement) {
-                this.createUserMessage();
-            }
-            if (this.birdle.modal.show) {
-                this.closeBirdleModal();
-            }
-        });
-        this.birdle.bird = this.birdle.wordBank[Math.floor(Math.random() * this.birdle.wordBank.length)];
     },
     beforeDestroy() {
         window.removeEventListener('scroll', this.handleScroll);
@@ -323,92 +314,6 @@ export default {
         },
         toggleRadMode() {
             this.radMode = !this.radMode;
-        },
-        copyShareLink() {
-            navigator.clipboard.writeText('Check out this strangely niche and much worse version of Wordle! ' + window.location.href);
-            this.birdle.linkCopied = true;
-        },
-        checkWord() {
-            let guess = this.birdle.guess;
-            this.axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`)
-                .then(response => {
-                    if (response.data[0].word === guess) this.makeGuess(guess);
-                }).catch(error => {
-                this.birdle.modal.notAWord = true;
-                this.birdle.modal.show = true;
-            });
-        },
-        enterPress() {
-            if (!this.birdle.modal.show) {
-                this.checkWord();
-            } else {
-                this.closeBirdleModal();
-                return false;
-            }
-        },
-        closeBirdleModal() {
-            this.birdle.modal.show = false;
-            setTimeout(() => {
-                this.birdle.modal.show = false;
-                this.birdle.modal.notAWord = false;
-                this.birdle.modal.guessRepeat = false;
-                this.birdle.modal.minLetters = false;
-            }, 1000);
-        },
-        birdleFocus() {
-            if (!this.birdle.gameOver) this.$refs.birdleInput.focus();
-        },
-        letterCheck(letter, index) {
-            if (letter === this.birdle.bird[index]) {
-                return 'correct-position';
-            } else if (this.birdle.bird.includes(letter)) {
-                return 'correct-letter';
-            } else {
-                return 'wrong-letter';
-            }
-        },
-        makeGuess() {
-            // Make sure the user has entered a 5-letter word
-            if (this.birdle.guess.length !== 5) {
-                this.birdle.modal.minLetters = true;
-                this.birdle.modal.show = true;
-                return;
-            }
-            // Make sure the user hasn't already guessed this word
-            if (this.birdle.attempts.includes(this.birdle.guess)) {
-                this.birdle.modal.guessRepeat = true;
-                this.birdle.modal.show = true;
-                return;
-            }
-            // Add the guess to the attempts array
-            this.birdle.attempts.push(this.birdle.guess);
-            // Check if the user has won, lost, or still playing
-            if (this.birdle.guess === this.birdle.bird) {
-                this.birdle.gameWon = true;
-                this.birdle.gameOver = true;
-                this.birdle.modal.show = true;
-            } else if (this.remainingGuesses === 0) {
-                this.birdle.gameLost = true;
-                this.birdle.gameOver = true;
-                this.birdle.modal.show = true;
-            }
-            this.birdle.guess = "";
-        },
-        birdleReset() {
-            // Reset the game but don't use the same word twice in a row
-            let newWord = this.birdle.wordBank[Math.floor(Math.random() * this.birdle.wordBank.length)];
-            console.log('currentWord', this.birdle.bird);
-            console.log('newWord', newWord);
-            while (newWord === this.birdle.bird) {
-                newWord = this.birdle.wordBank[Math.floor(Math.random() * this.birdle.wordBank.length)];
-            }
-            this.birdle.bird = newWord;
-            this.birdle.guess = "";
-            this.birdle.attempts = [];
-            this.birdle.gameOver = false;
-            this.birdle.gameWon = false;
-            this.birdle.gameLost = false;
-            this.birdle.linkCopied = false;
         },
         dataCarouselClasses(data) {
             if (!this.isLoading) {
@@ -488,56 +393,6 @@ export default {
         activeDataTab() {
             return this.dataTabs.find(t => t.isActive);
         },
-        guessLettersArray() {
-            if (this.remainingGuesses > 0) return this.birdle.guess.split('').concat(Array(5 - this.birdle.guess.length).fill(''));
-        },
-        remainingGuesses() {
-            return 5 - this.birdle.attempts.length;
-        },
-        modalHeading() {
-            if (this.birdle.modal.guessRepeat) {
-                return 'Oops!';
-            } else if (this.birdle.modal.notAWord) {
-                return 'Sorry!';
-            } else if (this.birdle.modal.minLetters) {
-                return 'Keep Typing!';
-            } else if (this.birdle.gameLost) {
-                return 'You lost!!';
-            } else if (this.birdle.gameWon && this.remainingGuesses === 4) {
-                return 'You...won?';
-            } else if (this.birdle.gameWon) {
-                return 'You won!!';
-            } else {
-                return 'Keep Typing!';
-            }
-        },
-        modalContent() {
-            if (this.birdle.modal.guessRepeat) {
-                return 'You\'ve already tried that word';
-            } else if (this.birdle.modal.notAWord) {
-                return 'That\'s not a word';
-            } else if (this.birdle.modal.minLetters) {
-                return 'Your guess must be a five letter word!';
-            } else if (this.birdle.gameLost) {
-                return 'Tip: Try harder';
-            } else if (this.birdle.gameWon && this.remainingGuesses === 4) {
-                return 'Wow, first try! Did you cheat? Tell your friends about your amazing luck!';
-            } else if (this.birdle.gameWon) {
-                return 'Be sure to brag to your friends!';
-            } else {
-                return '';
-            }
-        }
-    },
-    watch: {
-        // if this.birdle.linkCopied is true, set it to false after 5 seconds
-        // 'birdle.linkCopied': function (val) {
-        //     if (val) {
-        //         setTimeout(() => {
-        //             this.birdle.linkCopied = false;
-        //         }, 2000);
-        //     }
-        // }
     }
 }
 </script>
--- src/components/Birdle.vue
+++ src/components/Birdle.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="birdleApp" @click="birdleFocus">
-        <div class="birdle-grid animate__animated" :class="{ 'fade': birdle.modal.show }">
+        <div class="birdle-grid animate__animated" :class="{ 'fade': this.modal.show }">
             <div class="attempt row animate__animated"
-                 :class="{ 'animate__heartBeat': attempt === birdle.bird }"
-                 v-for="(attempt, index) in birdle.attempts"
+                 :class="{ 'animate__heartBeat': attempt === this.bird }"
+                 v-for="(attempt, index) in this.attempts"
                  :key="index">
                 <div class="letter"
                      :class="letterCheck(letter, index)"
@@ -23,12 +23,12 @@
                 <div class="empty letter" v-for="letter in 5"></div>
             </div>
         </div>
-        <div class="birdle-modal animate__animated" :class="this.birdle.modal.show ? 'animate__bounceIn' : 'animate__bounceOut'">
+        <div class="birdle-modal animate__animated" :class="this.modal.show ? 'animate__bounceIn' : 'animate__bounceOut'">
             <div class="birdle-modal__container">
                 <div class="birdle-modal__title" v-text="modalHeading"></div>
                 <div class="birdle-modal__content" v-text="modalContent"></div>
-                <div v-if="birdle.gameWon" class="share-link-btn" :class="{ 'copied': birdle.linkCopied }" @click="copyShareLink" v-text="birdle.linkCopied ? 'Link Copied!' : 'Copy Share Link'"></div>
-                <em v-if="birdle.gameLost">The answer was "{{ birdle.bird }}"</em>
+                <div v-if="this.modal.gameWon" class="share-link-btn" :class="{ 'copied': this.linkCopied }" @click="copyShareLink" v-text="this.linkCopied ? 'Link Copied!' : 'Copy Share Link'"></div>
+                <em v-if="this.modal.gameLost">The answer was "{{ this.bird }}"</em>
             </div>
             <div class="modal-close" @click="closeBirdleModal">
                 <i class="fa-solid fa-times"></i>
@@ -48,11 +48,11 @@
                 <div class="legend-item__text">Wrong Letter</div>
             </div>
         </div>
-        <div class="input" v-if="!birdle.gameOver" onkeydown="return /[a-z]/i.test(event.key)">
-            <input v-model="birdle.guess" ref="birdleInput" maxlength="5" type="text" @keydown.enter.prevent="enterPress"/>
+        <div class="input" v-if="!this.modal.gameOver" onkeydown="return /[a-z]/i.test(event.key)">
+            <input v-model="this.guess" ref="birdleInput" maxlength="5" type="text" @keydown.enter.prevent="enterPress"/>
             <button @click="checkWord">Submit</button>
         </div>
-        <div class="restart button" v-if="birdle.gameOver">
+        <div class="restart button" v-if="this.modal.gameOver">
             <div @click="birdleReset">Play Again</div>
         </div>
     </div>
@@ -63,43 +63,56 @@ export default {
     name: "Birdle",
     data() {
         return {
-            birdle: {
-                bird: '',
-                guess: "",
-                attempts: [],
-                modal: {
-                    show: false,
-                    notAWord: false,
-                    guessRepeat: false,
-                    minLetters: false,
-                    gameOver: false,
-                    gameLost: false,
-                    gameWon: false,
-                },
-                linkCopied: false,
+            bird: '',
+            guess: "",
+            attempts: [],
+            modal: {
+                show: false,
+                notAWord: false,
+                guessRepeat: false,
+                minLetters: false,
+                gameOver: false,
+                gameLost: false,
+                gameWon: false,
             },
+            linkCopied: false,
+            wordBank: [
+                "eagle", "heron", "robin", "crane", "swift", "finch", "stork", "lark",
+                "tern", "kite", "dove", "egret", "wren", "snipe", "quail", "scaup",
+                "skua", "goose", "coot", "trogon", "pewee", "merle", "swan", "ouzel",
+                "vireo", "knot", "ibis", "condor", "piper", "macaw", "puffin", "grebe",
+                "junco", "heron", "eider", "saker", "finch", "serin", "corre", "owlet"
+            ],
         }
     },
-    props: {
-        wordBank: Array
+    mounted() {
+        document.addEventListener('keydown', (event) => {
+            if (event.keyCode === 13 && this.$refs.chatInput === document.activeElement) {
+                this.createUserMessage();
+            }
+            if (this.modal.show) {
+                this.closeBirdleModal();
+            }
+        });
+        this.bird = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
     },
     methods: {
         copyShareLink() {
             navigator.clipboard.writeText('Check out this strangely niche and much worse version of Wordle! ' + window.location.href);
-            this.birdle.linkCopied = true;
+            this.linkCopied = true;
         },
         checkWord() {
-            let guess = this.birdle.guess;
+            let guess = this.guess;
             this.axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`)
                 .then(response => {
                     if (response.data[0].word === guess) this.makeGuess(guess);
                 }).catch(error => {
-                this.birdle.modal.notAWord = true;
-                this.birdle.modal.show = true;
+                this.modal.notAWord = true;
+                this.modal.show = true;
             });
         },
         enterPress() {
-            if (!this.birdle.modal.show) {
+            if (!this.modal.show) {
                 this.checkWord();
             } else {
                 this.closeBirdleModal();
@@ -107,21 +120,21 @@ export default {
             }
         },
         closeBirdleModal() {
-            this.birdle.modal.show = false;
+            this.modal.show = false;
             setTimeout(() => {
-                this.birdle.modal.show = false;
-                this.birdle.modal.notAWord = false;
-                this.birdle.modal.guessRepeat = false;
-                this.birdle.modal.minLetters = false;
+                this.modal.show = false;
+                this.modal.notAWord = false;
+                this.modal.guessRepeat = false;
+                this.modal.minLetters = false;
             }, 1000);
         },
         birdleFocus() {
-            if (!this.birdle.gameOver) this.$refs.birdleInput.focus();
+            if (!this.modal.gameOver) this.$refs.birdleInput.focus();
         },
         letterCheck(letter, index) {
-            if (letter === this.birdle.bird[index]) {
+            if (letter === this.bird[index]) {
                 return 'correct-position';
-            } else if (this.birdle.bird.includes(letter)) {
+            } else if (this.bird.includes(letter)) {
                 return 'correct-letter';
             } else {
                 return 'wrong-letter';
@@ -129,84 +142,84 @@ export default {
         },
         makeGuess() {
             // Make sure the user has entered a 5-letter word
-            if (this.birdle.guess.length !== 5) {
-                this.birdle.modal.minLetters = true;
-                this.birdle.modal.show = true;
+            if (this.guess.length !== 5) {
+                this.modal.minLetters = true;
+                this.modal.show = true;
                 return;
             }
             // Make sure the user hasn't already guessed this word
-            if (this.birdle.attempts.includes(this.birdle.guess)) {
-                this.birdle.modal.guessRepeat = true;
-                this.birdle.modal.show = true;
+            if (this.attempts.includes(this.guess)) {
+                this.modal.guessRepeat = true;
+                this.modal.show = true;
                 return;
             }
             // Add the guess to the attempts array
-            this.birdle.attempts.push(this.birdle.guess);
+            this.attempts.push(this.guess);
             // Check if the user has won, lost, or still playing
-            if (this.birdle.guess === this.birdle.bird) {
-                this.birdle.gameWon = true;
-                this.birdle.gameOver = true;
-                this.birdle.modal.show = true;
+            if (this.guess === this.bird) {
+                this.modal.gameWon = true;
+                this.modal.gameOver = true;
+                this.modal.show = true;
             } else if (this.remainingGuesses === 0) {
-                this.birdle.gameLost = true;
-                this.birdle.gameOver = true;
-                this.birdle.modal.show = true;
+                this.modal.gameLost = true;
+                this.modal.gameOver = true;
+                this.modal.show = true;
             }
-            this.birdle.guess = "";
+            this.guess = "";
         },
         birdleReset() {
             // Reset the game but don't use the same word twice in a row
-            let newWord = this.props.wordBank[Math.floor(Math.random() * this.props.wordBank.length)];
-            console.log('currentWord', this.birdle.bird);
+            let newWord = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+            console.log('currentWord', this.bird);
             console.log('newWord', newWord);
-            while (newWord === this.birdle.bird) {
+            while (newWord === this.bird) {
                 newWord = this.props.wordBank[Math.floor(Math.random() * this.props.wordBank.length)];
             }
-            this.birdle.bird = newWord;
-            this.birdle.guess = "";
-            this.birdle.attempts = [];
-            this.birdle.gameOver = false;
-            this.birdle.gameWon = false;
-            this.birdle.gameLost = false;
-            this.birdle.linkCopied = false;
+            this.bird = newWord;
+            this.guess = "";
+            this.attempts = [];
+            this.modal.gameOver = false;
+            this.modal.gameWon = false;
+            this.modal.gameLost = false;
+            this.linkCopied = false;
         }
     },
     computed: {
         guessLettersArray() {
-            if (this.remainingGuesses > 0) return this.birdle.guess.split('').concat(Array(5 - this.birdle.guess.length).fill(''));
+            if (this.remainingGuesses > 0) return this.guess.split('').concat(Array(5 - this.guess.length).fill(''));
         },
         remainingGuesses() {
-            return 5 - this.birdle.attempts.length;
+            return 5 - this.attempts.length;
         },
         modalHeading() {
-            if (this.birdle.modal.guessRepeat) {
+            if (this.modal.guessRepeat) {
                 return 'Oops!';
-            } else if (this.birdle.modal.notAWord) {
+            } else if (this.modal.notAWord) {
                 return 'Sorry!';
-            } else if (this.birdle.modal.minLetters) {
+            } else if (this.modal.minLetters) {
                 return 'Keep Typing!';
-            } else if (this.birdle.gameLost) {
+            } else if (this.modal.gameLost) {
                 return 'You lost!!';
-            } else if (this.birdle.gameWon && this.remainingGuesses === 4) {
+            } else if (this.modal.gameWon && this.remainingGuesses === 4) {
                 return 'You...won?';
-            } else if (this.birdle.gameWon) {
+            } else if (this.modal.gameWon) {
                 return 'You won!!';
             } else {
                 return 'Keep Typing!';
             }
         },
         modalContent() {
-            if (this.birdle.modal.guessRepeat) {
+            if (this.modal.guessRepeat) {
                 return 'You\'ve already tried that word';
-            } else if (this.birdle.modal.notAWord) {
+            } else if (this.modal.notAWord) {
                 return 'That\'s not a word';
-            } else if (this.birdle.modal.minLetters) {
+            } else if (this.modal.minLetters) {
                 return 'Your guess must be a five letter word!';
-            } else if (this.birdle.gameLost) {
+            } else if (this.modal.gameLost) {
                 return 'Tip: Try harder';
-            } else if (this.birdle.gameWon && this.remainingGuesses === 4) {
+            } else if (this.modal.gameWon && this.remainingGuesses === 4) {
                 return 'Wow, first try! Did you cheat? Tell your friends about your amazing luck!';
-            } else if (this.birdle.gameWon) {
+            } else if (this.modal.gameWon) {
                 return 'Be sure to brag to your friends!';
             } else {
                 return '';
